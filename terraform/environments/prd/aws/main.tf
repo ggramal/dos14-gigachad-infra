be0198acdec3cd3f590be26917b07fe62c171e4c
Gramovich Alexander <gramovich.alex@outlook.com>
2023-09-08 11:43:30 +0300

    Remove route53 + test vpc

--- terraform/environments/prd/aws/main.tf
+++ terraform/environments/prd/aws/main.tf
@@ -17,42 +17,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-locals {
-  instances = {
-    "instance1" = {
-      name = "tf-example-1"
-    }
-  }
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "authz" {
-  for_each               = local.instances
-  ami           = data.aws_ami.ubuntu.image_id
-  instance_type = "t3.micro"
-  key_name = "gae"
-  subnet_id = module.vpcs["gigachad-tf"].subnets["public-subnet1"].id
-  vpc_security_group_ids = ["sg-0905d0ff7aa4d44fd"]
-  tags = {
-    Name = each.value.name
-  }
-}
-
 
 module "vpcs" {
   source =  "../../../modules/aws/vpc/"
@@ -63,4 +27,3 @@ module "vpcs" {
   nat_gws = each.value.nat_gws
   subnets = each.value.subnets
 }
-
--- terraform/environments/prd/aws/vpc_variables.tf
+++ terraform/environments/prd/aws/vpc_variables.tf
@@ -40,45 +40,5 @@ locals {
         }
       }
     }
-    gigachad-tf-2 = {
-      name = "gigachad-tf-2"
-      cidr = "10.0.0.0/16"  
-      internet_gws = {
-        "igw-1" = {
-          name = "igw-1"
-        }
-      }
-      nat_gws = {
-        "nat-1" = {
-          name = "nat-1"
-          subnet = "private-subnet1"
-        }
-      }
-      subnets = {
-        public-subnet1 = {
-          name = "public-subnet1"
-          cidr = "10.0.3.0/24"
-          public_ip_on_launch = true
-          availability_zone = "eu-west-2a"
-          routes = [
-            {
-              cidr = "0.0.0.0/0"
-              internet_gw = "igw-1"
-            }
-          ]
-        }
-        private-subnet1 = {
-          name = "private-subnet1"
-          cidr = "10.0.4.0/24"
-          availability_zone = "eu-west-2c"
-          routes = [
-            {
-              cidr = "0.0.0.0/0"
-              nat_gw = "nat-1"
-            }
-          ]
-        }
-      }
-    }
   }
 }
