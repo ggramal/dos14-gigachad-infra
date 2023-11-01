93ba7726e96f4d2548dc1e0b5bf46eb8b704fcf0
GosuOoops <111736684+GosuOoops@users.noreply.github.com>
2023-11-01 17:12:48 +0300

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
+read -p "Enter your aws user: " aws_user
 read -p 'Enter your MFA token: ' token
 read -p 'Enter profile name: ' profile
 read -p 'Enter mfa name: ' mfa
-export AWS_PROFILE=$profile
-accountID=$( aws sts get-caller-identity --profile $profile | jq -r '.Account')
-json=$( aws sts assume-role --role-arn arn:aws:iam::$accountID:role/Admins \
---role-session-name tf \
---serial-number arn:aws:iam::$accountID:mfa/$mfa \
---token-code $token \
---profile $profile)
-eval $(echo  $json | jq -r '.Credentials | "AccessKeyId=\(.AccessKeyId)","SecretAccessKey=\(.SecretAccessKey)","SessionToken=\(.SessionToken)"')
+
+caller_identity=$(aws sts --profile $profile get-caller-identity)
+AccountID=$(echo "$caller_identity" | jq -r '.Account')
+
+output_aws=$(aws sts assume-role --role-arn arn:aws:iam::"$AccountID":role/Admins --role-session-name "$aws_user"\
+ --serial-number arn:aws:iam::"$AccountID":mfa/"$mfa" --token-code "$token" --profile "$profile")
+
+AccessKeyId=$(echo "$output_aws" | jq -r '.Credentials.AccessKeyId')
+SecretAccessKey=$(echo "$output_aws" | jq -r '.Credentials.SecretAccessKey')
+SessionToken=$(echo "$output_aws" | jq -r '.Credentials.SessionToken')
+
 export AWS_ACCESS_KEY_ID=$AccessKeyId
 export AWS_SECRET_ACCESS_KEY=$SecretAccessKey
 export AWS_SESSION_TOKEN=$SessionToken
