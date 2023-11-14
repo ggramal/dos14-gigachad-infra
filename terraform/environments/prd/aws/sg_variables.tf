8f79ec09dc8f7f308bcadd81db8ec761d17cc87e
Dmitriy <sphynxx.mail@gmail.com>
2023-11-14 14:59:58 +0000

    changed local variables

--- terraform/environments/prd/aws/sg_variables.tf
+++ terraform/environments/prd/aws/sg_variables.tf
@@ -3,46 +3,52 @@ locals {
       sgroup-1 = {
         name = "gigachad-sg-1"
         description = "Custom SG #1"
-        # vpc_id = data.aws_vpc.main.vpc_id
-        ingresses = {
-          ingress-1 = {
-              from_port = 22
-              to_port = 22
-              protocol = "TCP"
-            }
-          ingress-2 = {
-              from_port = 80
-              to_port = 80
-              protocol = "TCP"
-              cidr_blocks = ["0.0.0.0/0"]
-            }
-        }
-        egress = {
-          egress-1 = {
+        ingresses = [
+          {
+            from_port = 22
+            to_port = 22
+            protocol = "TCP"
+          },
+          {
+            from_port = 80
+            to_port = 80
+            protocol = "TCP"
+            cidr_blocks = ["0.0.0.0/0"]
+          }
+        ]
+        egress = [
+          {
             from_port = 0
             to_port = 0
             protocol = -1
           }
-        }
+        ]
       }
       sgroup-2 = {
         name = "gigachad-sg-2"
         description = "Custom SG #2"
-        # vpc_id = data.aws_vpc.main.vpc_id
-        ingresses = {
-          ingress-1 = {
+        ingresses = [
+          {
             from_port = 1
             to_port = 22
             protocol = "TCP"
+            cidr_blocks = ["0.0.0.0/0"]
+          },
+          {
+            from_port = 80
+            to_port = 88
+            protocol = "TCP"
+            cidr_blocks = ["0.0.0.0/0"]
           }
-        }
-        egress = {
-          egress-1 = {
+        ]
+        egress = [
+          {
             from_port = 0
             to_port = 65535
             protocol = "TCP"
+            cidr_blocks = ["10.0.0.0/8"]
           }
-        }
+        ]
       }   
     }
 }
